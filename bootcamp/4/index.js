4e1ca7c015ccc9fd124bbe5fe3609fe9a9e3fbd5
vikkyroth <vikkyroth@gmail.com>
2024-02-05 21:09:13 +0700

    excersise

---
+++ bootcamp/4/index.js
@@ -0,0 +1,14 @@
+let listsepatu = ['nike', 'puma', 'adidas'];
+console.log(listsepatu[1]);
+
+let listpeople = [
+    {
+        name: 'ani',
+        age: 31,
+    },
+    {
+        name: 'vikky',
+        age: 30,
+    }
+];
+console.log(listpeople);
