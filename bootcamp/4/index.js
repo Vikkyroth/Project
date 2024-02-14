00b5af26d9f136d615084431e70b1b4dcc48bfce
vikkyroth <vikkyroth@gmail.com>
2024-02-14 21:11:19 +0700

    exer

--- bootcamp/4/index.js
+++ bootcamp/4/index.js
@@ -1,5 +1,6 @@
 let listsepatu = ['nike', 'puma', 'adidas'];
 console.log(listsepatu[1]);
+//array mulai dari 0
 
 let listpeople = [
     {
@@ -12,3 +13,50 @@ let listpeople = [
     }
 ];
 console.log(listpeople);
+
+function pertambahan(a, b){
+    return a + b;
+}
+
+console.log (pertambahan(100, 50))
+
+//nested function
+
+function message(name) {
+    function hello(){
+        return `Hello ${name},` ;
+    }
+
+    function welcome(){
+        return ` Welcome to Purwadika`
+    }
+    return hello() + welcome()
+}
+console.log(message("Vikky"));
+
+//Currying Function
+
+function multicur(factor){
+    return function(number){
+        return number * factor;
+    }
+}
+
+const result = multicur(5)
+console.log(result(3));
+
+//recursice function
+
+function countdown(message) {
+    console.log(message);
+
+    let nextnumber = message - 1;
+    if (nextnumber > 0) {
+        countdown(nextnumber);
+    }
+}
+
+countdown (4)
+
+//arrow function
+
---
+++ bootcamp/Exercise/exercise1.js
@@ -0,0 +1,11 @@
+//nomer 1
+let width = 3
+let length = 5
+const luas = width * length
+console.log(luas)
+
+//nomer 2
+
+const keliling = (width + length) *2
+console.log(keliling)
+
