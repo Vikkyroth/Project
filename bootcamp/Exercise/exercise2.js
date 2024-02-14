fe963ab4457188222b3f3d1d73f18ab5f7f981d7
vikkyroth <vikkyroth@gmail.com>
2024-02-14 20:50:40 +0700

    exer

---
+++ bootcamp/Exercise/exercise2.js
@@ -0,0 +1,40 @@
+//nomer 1
+const celcius = 60
+let farenheit = ((celcius * 9 / 5) + 32 );
+console.log (farenheit)
+
+//nomer 2
+const numbercheck = 25;
+let result;
+
+if (numbercheck % 2 === 0){
+    result = `${numbercheck} is even number`;}
+    else {
+    result = `${numbercheck} is odd number`;
+};
+console.log (result);
+
+//nomer 3
+
+
+
+//nomer 4
+let number = 5;
+let jumlah = 0;
+
+for(let i = 0; i <= number; i++){
+    jumlah += i;
+
+}
+console.log(jumlah)
+
+//nomer 5
+let number1 = 4;
+let jumblah = 1;
+
+for(let i = number1; i >= 1; i--){
+    jumblah *= i;
+}
+console.log(jumblah)
+
+//
---
+++ bootcamp/Exercise/exercise3.js
@@ -0,0 +1,17 @@
+//nomer 1
+let num = 9
+const mult = 10
+
+for(let i = 1; i <= mult; i++){
+    message = `${num} x ${i} = ${num*i}`;
+    console.log(message)
+}
+
+//nomer 2
+
+
+//nomer 3
+let cm = 100000
+let km = cm / 100000
+message = `${cm} = ${km} Km`
+console.log(message)
