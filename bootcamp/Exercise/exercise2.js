0faba968e71621c27a69ca85c6ea39575079a8b1
vikkyroth <vikkyroth@gmail.com>
2024-02-26 20:31:37 +0700

    test

--- bootcamp/Exercise/exercise2.js
+++ bootcamp/Exercise/exercise2.js
@@ -15,8 +15,19 @@ if (numbercheck % 2 === 0){
 console.log (result);
 
 //nomer 3
+let isPrime = true;
+message = `${numbercheck} is a prime number`;
 
+if (numbercheck === 1) message = `please input number other than 1`;
 
+for (let i = 2; i < numbercheck; i++) {
+  if (numbercheck % i == 0) {
+    isPrime = false;
+    message = `${numbercheck} is not a prime number`;
+    break;
+  }
+}
+console.log(message)
 
 //nomer 4
 let number = 5;
@@ -37,4 +48,18 @@ for(let i = number1; i >= 1; i--){
 }
 console.log(jumblah)
 
-//
+//nomer 6
+
+calc = 0;
+numberN = 10;
+let num1 = 0;
+let num2 = 1;
+message = `First ${numberN} of fibonacci = ${num1}, `;
+
+for (let i = 1; i <= numberN; i++) {
+  calc = num1 + num2;
+  num1 = num2;
+  num2 = calc;
+  message += i == 1 ? `${num1}` : `, ${num1}`;
+}
+console.log(message);
--- bootcamp/Exercise/exercise3.js
+++ bootcamp/Exercise/exercise3.js
@@ -8,10 +8,24 @@ for(let i = 1; i <= mult; i++){
 }
 
 //nomer 2
+let string = "amsma";
+console.log(string.split("").reverse().join(""));
+let stringLength = string.length;
+message = `${string} is a palindrome`;
 
+for (let i = 0; i < stringLength / 2; i++) {
+  if (string[i] !== string[stringLength - 1 - i]) {
+    message = `${string} is not a palindrome`;
+    break;
+  }
+}
+console.log(message);
 
 //nomer 3
 let cm = 100000
 let km = cm / 100000
 message = `${cm} = ${km} Km`
 console.log(message)
+
+//nomer 4
+
